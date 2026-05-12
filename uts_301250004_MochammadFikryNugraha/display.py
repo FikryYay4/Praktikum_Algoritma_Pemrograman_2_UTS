46cd0f3853898539272c14846c13fdee70579f3e
FikryYay4 <146968366+FikryYay4@users.noreply.github.com>
2026-05-12 16:01:34 +0700

    Delete uts_301250004_MochammadFikryNugraha directory

--- uts_301250004_MochammadFikryNugraha/display.py
+++
@@ -1,5 +0,0 @@
-def tampilkan_display():
-    print("=== Aritmatika === ")
-    print("1. Penjumlahan digit Bilangan")
-    print("2. Menghitung perpangkatan Bilangan")
-    print("3. Keluar")
--- uts_301250004_MochammadFikryNugraha/penjumlahan_digitBilangan.py
+++
@@ -1,5 +0,0 @@
-def jumlah_digit_bilangan(n):
-    if n == 0:
-        return 0
-    else:
-        return (n % 10) + jumlah_digit_bilangan(n // 10)
